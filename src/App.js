816add705176c6ba9bd17c537eba66a7086bfc60
Tomsylva <tomsylvanz@gmail.com>
2021-05-01 15:36:00 +0200

    SmallDiv moved to different folder

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import SmallDiv from "./SmallDiv.js";
 
 function Navbar() {
   return (
@@ -62,23 +63,6 @@ function BottomSection() {
         title="JSX"
         text="Statically-typed, designed to run on modern browsers."
       />
-    </div>
-  );
-}
-
-function SmallDiv(props) {
-  return (
-    <div
-      style={{
-        width: "15%",
-        display: "flex",
-        flexDirection: "column",
-        padding: "110px 1px",
-      }}
-    >
-      <img src={props.image} alt="not found" />
-      <h3 style={{ margin: "0", fontSize: "21px" }}>{props.title}</h3>
-      <p style={{ fontSize: "15px", color: "gray" }}>{props.text}</p>
     </div>
   );
 }
