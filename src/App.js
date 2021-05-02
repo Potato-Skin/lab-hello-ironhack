34cd8870c0adfa320e44805624f4d6460bfb8b16
jahfunk <xknottykidx@gmail.com>
2021-05-02 23:30:26 +0200

    this was fun

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+function Navbar(props) {
+  return (
+    <div
+      style={{
+        background: "black",
+        display: "flex",
+        justifyContent: "space-between",
+      }}
+    >
+      <img src="/images/ironhack-logo.svg"></img>
+      <img src="/images/menu-top.svg"></img>
+    </div>
+  );
+}
+
+function Footer(props) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-evenly",
+      }}
+    >
+      <div
+        style={{
+          width: "100px",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <img src="../images/icon1.png" />
+        <h3> Declarative</h3>
+        <p> React makes it painless to create interative Uls</p>
+      </div>
+      <div
+        style={{
+          width: "100px",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <img src="../images/icon2.png" />
+        <h3> Components</h3>
+        <p> Build encapsulated components that manage their state.</p>
+      </div>
+      <div
+        style={{
+          width: "100px",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <img src="../images/icon3.png" />
+        <h3> Single-Way</h3>
+        <p> A set of immutable values are passed to the component's.</p>
+      </div>
+      <div
+        style={{
+          width: "100px",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <img src="../images/icon4.png" />
+        <h3> JSX</h3>
+        <p> Statically-typed, designed to run on modern browsers.</p>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Say hello to ReactJS</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          You will learn how to use the most popular frontend library, and
+          become a super Ninja developer
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button>Awesome!</button>
       </header>
+      <Footer />
     </div>
   );
 }
